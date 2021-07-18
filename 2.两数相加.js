c4b090196c0bbe1e80f7f55fb66df36aad169a14
Chris_zy <1633711653@qq.com>
2021-07-18 14:12:42 +0800

    add test 2

--- 2.两数相加.js
+++ 2.两数相加.js
@@ -17,8 +17,31 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-
+var addTwoNumbers = function (l1, l2) {
+    let head = tail = null
+    let carry = 0
+    while (l1 || l2) {
+        const v1 = l1 ? l1.val : 0
+        const v2 = l2 ? l2.val : 0
+        const sum = v1 + v2 + carry
+        if (!head) {
+            head = tail = new ListNode(sum % 10)
+        } else {
+            tail.next = new ListNode(sum % 10)
+            tail = tail.next
+        }
+        carry = Math.floor(sum / 10)
+        if(l1){
+            l1 = l1.next
+        }
+        if(l2){
+            l2 = l2.next
+        }
+    }
+    if(carry > 0){
+        tail.next = new ListNode(carry)
+    }
+    return head
 };
 // @lc code=end
 
